22cd6b70d4e955cbc5207601381988faadc1b588
marthaberner <marthaberner.mb@gmail.com>
2015-07-08 10:31:03 -0600

    setTimeout

--- examples/07_later.js
+++ examples/07_later.js
@@ -11,3 +11,17 @@ Execute `laterLater` passing it an anonymous function that console logs the argu
 Execute `laterLater` passing it a variable that points to function that console logs the arguments.
 
 */
+
+function laterLater(callback, arg1, arg2) {
+  setTimeout(callback(arg1, arg2), 3000);
+}
+
+laterLater(function(arg1, arg2) {
+  console.log(arg1, arg2);
+}, "in 3 seconds", "I appear");
+
+var myFunc = function(arg1, arg2) {
+  console.log(arg1, arg2);
+}
+
+laterLater(myFunc, "I am a named func", "goodbye");
