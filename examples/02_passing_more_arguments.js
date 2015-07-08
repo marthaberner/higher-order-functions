be8d38ff19ac2733e3fc6e57c5134fc933ac67f6
marthaberner <marthaberner.mb@gmail.com>
2015-07-08 09:45:14 -0600

    passing multiple arguments

--- examples/02_passing_more_arguments.js
+++ examples/02_passing_more_arguments.js
@@ -11,3 +11,21 @@ Execute `hitMeBack` passing an anonymous function that console logs its argument
 Execute `hitMeBack` passing a variable that points to a function that console logs its arguments
 
 */
+
+function hitMeBack (cb, arg1, arg2, arg3) {
+  cb(arg1, arg2, arg3);
+};
+
+hitMeBack(function(arg1, arg2, arg3) {
+  console.log(arg1);
+  console.log(arg2);
+  console.log(arg3);
+}, "Banana", "apple", 23);
+
+var myFunc = function(arg1, arg2, arg3) {
+  console.log(arg1);
+  console.log(arg2);
+  console.log(arg3);
+}
+
+hitMeBack(myFunc, "HELLO", "GOODBYE", 15);
