10ea35cd8cf6cf7445592a8e47aa65417ba92870
marthaberner <marthaberner.mb@gmail.com>
2015-07-08 19:38:43 -0600

    remove unused variables

--- routes/index.js
+++ routes/index.js
@@ -3,19 +3,19 @@ var router = express.Router();
 var goGet = require('../lib/data');
 
 router.get('/', function(req, res, next) {
-  var cities = goGet.cities(function(cities) {
+  goGet.cities(function(cities) {
     res.render('index', { cities: cities });
   });
 });
 
 router.get('/cities/:city_id', function(req, res, next) {
-  var city = goGet.city(function(city) {
+  goGet.city(function(city) {
     res.render('show', { city: city });
   }, req.params.city_id);
 });
 
 router.get('/weather', function(req, res, next) {
-  var weather = goGet.weather(function(weather){
+  goGet.weather(function(weather){
     res.render('weather', { data: weather });
   });
 });
